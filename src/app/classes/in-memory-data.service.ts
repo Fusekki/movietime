d9a8b7ff1a14aaf501f811d1ddc31922f0ce94cc
Phillip Stafford <phillip@philliprstafford.com>
2018-05-09 15:30:39 -0400

    feat: Add service to house mock database entries
    Add createdDB method with user object definition.

---
+++ src/app/classes/in-memory-data.service.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+    created = Date.now();
+
+  createDb() {
+    const users = [
+      {id: 0, email: 'janedoe@gmail.com',
+                      firstName: 'Jane', lastName: 'Doe' }
+    ];
+
+    return {
+      users
+      };
+  }
+}
+
