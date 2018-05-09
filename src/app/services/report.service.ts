e55035d5563173be14e9ddda87ead99f99faa858
Phillip Stafford <phillip@philliprstafford.com>
2018-05-09 16:35:25 -0400

    feat: Add reportService.  Create addReport, clear and getReports().

---
+++ src/app/services/report.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ReportService {
+
+  reports: string[] = [];
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+
+  clear() {
+    this.reports = [];
+  }
+
+  getReports(): string[] {
+    return this.reports;
+  }
+}
