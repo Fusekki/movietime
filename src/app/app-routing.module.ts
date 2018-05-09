74bffbc6969fb417e3bd3617fb94acdfc390b1c8
Phillip Stafford <phillip@philliprstafford.com>
2018-05-09 17:16:50 -0400

    feat: Add paths for profile and wildcard
    Import AppDashboard and PageNotFound components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
-// import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppDashboardComponent } from './components/app-dashboard/app-dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: ':id/profile', component: AppDashboardComponent },
   // These defaults need to be last
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
-  // { path: '**', component: PageNotFoundComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
