f0cab48d855890249a349fc0fbe9f47d0a5167b2
Phillip Stafford <phillip@philliprstafford.com>
2018-05-10 14:05:41 -0400

    refactor: Replace app-dashboard with dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
-import { AppDashboardComponent } from './components/app-dashboard/app-dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: ':id/profile', component: AppDashboardComponent },
+  { path: ':id/profile', component: DashboardComponent },
   // These defaults need to be last
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
