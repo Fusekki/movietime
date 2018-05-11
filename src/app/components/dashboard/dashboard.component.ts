69ed15f4a0e1b03c67eed1527d282b91bd8d0a00
Phillip Stafford <phillip@philliprstafford.com>
2018-05-11 11:46:22 -0400

    style: Beautify ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,15 +3,20 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
-import {FormControl, FormGroupDirective, NgForm, Validators, FormBuilder} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { UserService } from '../../services/user.service';
 import { User } from '../../classes/user';
-
 
 @Component({
   selector: 'app-dashboard',
@@ -19,29 +24,34 @@ import { User } from '../../classes/user';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
   user: User;
   zipPattern = /^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$/;
 
-  zipcode = new FormControl('', [Validators.required, Validators.pattern(this.zipPattern)]);
+  zipcode = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.zipPattern)
+  ]);
 
   getErrorMessage() {
-    return this.zipcode.hasError('required') ? 'You must enter a value' :
-    this.zipcode.hasError('pattern') ? 'Not a valid zipcode' : '';
+    return this.zipcode.hasError('required')
+      ? 'You must enter a value'
+      : this.zipcode.hasError('pattern')
+        ? 'Not a valid zipcode'
+        : '';
   }
 
-  constructor(private userService: UserService,
-  private router: Router,
-private route: ActivatedRoute,
-private fb: FormBuilder) { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
   ngOnInit() {
     this.getUser();
   }
 
   getUser(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.userService.getUser(id)
-      .subscribe(user => this.user = user);
+    this.userService.getUser(id).subscribe(user => (this.user = user));
   }
-
 }
