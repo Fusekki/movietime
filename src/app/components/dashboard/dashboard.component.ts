78c0cbfd60d4b2ce997896a929b4f81c65814820
Phillip Stafford <phillip@philliprstafford.com>
2018-05-11 11:36:26 -0400

    fix: Fix zipcode form validator

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormBuilder} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 
 import {MatInputModule} from '@angular/material/input';
@@ -12,12 +12,12 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../classes/user';
 
 /** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
 
 @Component({
   selector: 'app-dashboard',
@@ -27,18 +27,19 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 export class DashboardComponent implements OnInit {
 
   user: User;
+  zipPattern = /^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$/;
 
-  zipcodeFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern('/^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$/'),
-  ]);
+  zipcode = new FormControl('', [Validators.required, Validators.pattern(this.zipPattern)]);
 
-  matcher = new MyErrorStateMatcher();
+  getErrorMessage() {
+    return this.zipcode.hasError('required') ? 'You must enter a value' :
+    this.zipcode.hasError('pattern') ? 'Not a valid zipcode' : '';
+  }
 
   constructor(private userService: UserService,
   private router: Router,
-private route: ActivatedRoute) { }
-
+private route: ActivatedRoute,
+private fb: FormBuilder) { }
   ngOnInit() {
     this.getUser();
   }
