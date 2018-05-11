07286268b9de31b96286c9deae62cf8bf6a7a228
Phillip Stafford <phillip@philliprstafford.com>
2018-05-11 15:18:24 -0400

    feat: Add onSubmit function to redirect Also, format code.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -31,27 +31,12 @@ export class DashboardComponent implements OnInit {
   zipForm: FormGroup;
   zipPattern = /^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$/;
 
-  // onSubmit() { this.submitted = true; }
-
-  getErrorMessage() {
-    console.log(this.zipForm.controls.zipcode);
-    return this.zipForm.controls.zipcode.hasError('required')
-      ? 'You must enter a value'
-      : this.zipForm.controls.zipcode.hasError('minlength')
-        ? 'Too short'
-        : this.zipForm.controls.zipcode.hasError('maxlength')
-        ? 'Too Long'
-        : this.zipForm.controls.zipcode.hasError('pattern')
-        ? 'Not a valid zipcode'
-        : '';
-  }
-
   constructor(
     private userService: UserService,
     private router: Router,
     private route: ActivatedRoute,
     private fb: FormBuilder
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getUser();
@@ -66,7 +51,24 @@ export class DashboardComponent implements OnInit {
 
   createForm() {
     this.zipForm = this.fb.group({
-      zipcode: ['', [Validators.required, Validators.pattern(this.zipPattern), Validators.minLength(5), Validators.maxLength(10)] ]
+      zipcode: ['', [Validators.required, Validators.pattern(this.zipPattern), Validators.minLength(5), Validators.maxLength(10)]]
     });
+  }
+
+  getErrorMessage() {
+    console.log(this.zipForm.controls.zipcode);
+    return this.zipForm.controls.zipcode.hasError('required')
+      ? 'You must enter a value'
+      : this.zipForm.controls.zipcode.hasError('minlength')
+        ? 'Too short'
+        : this.zipForm.controls.zipcode.hasError('maxlength')
+          ? 'Too Long'
+          : this.zipForm.controls.zipcode.hasError('pattern')
+            ? 'Not a valid zipcode'
+            : '';
+  }
+
+  onSubmit(): void {
+    this.router.navigate(['./' + this.user.id + '/theaters']);
   }
 }
