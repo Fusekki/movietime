15831e31004a5e8eb4e6c9097949b5ce7c676345
Phillip Stafford <phillip@philliprstafford.com>
2018-05-15 14:35:03 -0400

    feat: Add starter code and methods to component

--- src/app/components/theater-search-results/theater-search-results.component.ts
+++ src/app/components/theater-search-results/theater-search-results.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { UserService } from '../../services/user.service';
+import { User } from '../../classes/user';
+
 
 @Component({
   selector: 'app-theater-search-results',
@@ -7,9 +17,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TheaterSearchResultsComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  zipcode: string;
+  username: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getUser();
+    this.getTheaters();
+
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('zipcode'));
+      this.zipcode = params.get('zipcode');
+    });
+  }
+
+
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('user');
+    this.userService.getUser(id).subscribe(user => (this.user = user));
+  }
+
+  getTheaters(): void {
   }
 
 }
