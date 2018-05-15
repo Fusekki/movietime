0e65a5418a48d6b3b7f6908f4cd0affcd1e747b1
Phillip Stafford <phillip@philliprstafford.com>
2018-05-15 13:40:40 -0400

    feat: Add route for theater-search-results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TheaterSearchResultsComponent } from './components/theater-search-results/theater-search-results.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: ':id/profile', component: DashboardComponent },
+  { path: ':id/theaters', component: TheaterSearchResultsComponent },
   // These defaults need to be last
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
