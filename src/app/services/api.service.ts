d514c4c9ce1672dbef15bd5ecb303d9e9a244ee2
Phillip Stafford <phillip@philliprstafford.com>
2018-05-16 12:36:32 -0400

    feat: Add getMovies method

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getMovies() {
+    const url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-05-16&zip=06820&api_key=3pb6pdpr5j5eeyucy9a5s5ua';
+    this.http.get(url)
+    .subscribe(
+      data => {console.log(data);
+    });
+  }
 }
