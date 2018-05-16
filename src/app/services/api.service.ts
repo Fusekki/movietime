e12c2b706de1b88d0afa989330b09793f5453c81
Phillip Stafford <phillip@philliprstafford.com>
2018-05-16 15:16:05 -0400

    feat: Add getTheaters method for mock data calls Comment out call to API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,17 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable ,  of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
+import { ReportService } from './report.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class ApiService {
+  constructor(private http: HttpClient,
+  private reportService: ReportService) { }
 
-  constructor(private http: HttpClient) { }
-  getMovies() {
-    const url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-05-16&zip=06820&api_key=3pb6pdpr5j5eeyucy9a5s5ua';
-    this.http.get(url)
-    .subscribe(
-      data => {console.log(data);
-    });
+  private theatersUrl = 'api/theaters';  // URL to web api
+
+  // Save this for when we go live
+
+  // getMovies() {
+  //   const url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-05-16&zip=06820&api_key=3pb6pdpr5j5eeyucy9a5s5ua';
+  //   return this.http.get(url);
+  // }
+
+  /** GET Useres from the server */
+  getTheaters(): Observable<any[]> {
+    return this.http.get<any[]>(this.theatersUrl)
+    .pipe(
+      tap(theaters => this.log(`theaters users`))
+    );
   }
+
+  
+  // Logs the content to the reportService
+  private log(content: string) {
+    if (!content) { return; }
+    this.reportService.addReport( content );
+
+  }
+
 }
