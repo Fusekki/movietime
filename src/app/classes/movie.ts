219f7d8f2c67480cd9c969f8f42561212c4d57fd
Phillip Stafford <phillip@philliprstafford.com>
2018-05-17 16:49:50 -0400

    feat: Add constructor to class

--- src/app/classes/movie.ts
+++ src/app/classes/movie.ts
@@ -1,4 +1,16 @@
 export class Movie {
+    constructor(t)  {
+        this.title = t;
+        this.releaseDate = '';
+        this.genres = [];
+        this.cast = [];
+        this.directors = [];
+        this.shortDescription = '';
+        this.rated = '';
+        this.advisories = [];
+        this.runTime = '';
+        this.theaters = [];
+     }
     title: string;
     releaseDate: string;
     genres: string[];
