aa44ae85a26a7f3932837a1b4445d3b26b46706f
Phillip Stafford <phillip@philliprstafford.com>
2018-05-17 16:50:59 -0400

    fix: Resolve parseMovies function to parse movies into an array Movies

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable ,  of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
+
+import { Movie } from '../classes/movie';
+import { Theater } from '../classes/theater';
 import { Area } from '../classes/area';
 import { ReportService } from './report.service';
 
@@ -14,25 +17,40 @@ const httpOptions = {
 @Injectable()
 export class MovieService {
 
+  movies: any[] = [];
+
   constructor(private http: HttpClient,
     private reportService: ReportService) { }
 
-  parseMovies(data: string[]): void {
-    console.log(data);
-  //   this.tempMovies = [];
-  //   for (let movie of this.movies) {
-  //     // Check if the key exists, if not create it.
-  //     if (!this.tempMovies.includes(movie.title)) {
-  //       this.tempMovies['title'] = movie.title;
-  //     }
-  //     for (let showtime of movie.showtimes) {
-  //       console.log(showtime.theatre.name);
-  //       if this.tempMovies[]
-  //       console.log(showtime.dateTime);
-  //     }
-  //     // console.log(entry.showtimes);
-  //   }
-  //   console.log(this.tempMovies);
-  // }
+  parseMovies(data: any[]): void {
+    // console.log(data);
+    this.movies = [];
+    let theaters = [];
+    let current = Movie;
+    for (let movie of data) {
+      // console.log(movie.title);
+      current = new Movie(movie.title);
 
+      for (let showtime of movie.showtimes) {
+        // Check if theater exists, if not create it
+        // if (current.theaters.length > 0) {
+        let t = current.theaters.filter(x => x.name == showtime.theatre.name);
+        // console.log(t);
+        if (t.length > 0) {
+          // console.log('found');
+          // console.log(t);
+          t[0].times.push(showtime.dateTime);
+        } else {
+          // console.log('not found');
+          current.theaters.push({
+            name: showtime.theatre.name,
+            times: [showtime.dateTime]
+          });
+        }
+
+      }
+      this.movies.push(current);
+    }
+    console.log(this.movies);
+  }
 }
