a2323dc1249afcd1e39fe375604714a6b7555d70
Phillip Stafford <phillip@philliprstafford.com>
2018-05-17 13:57:57 -0400

    feat: Add movie class

--- src/app/classes/movie.ts
+++ src/app/classes/movie.ts
@@ -1,2 +1,12 @@
 export class Movie {
+    title: string;
+    releaseDate: string;
+    genres: string[];
+    cast: string[];
+    directors: string[];
+    shortDescription: string;
+    rated: string;
+    advisories: string[];
+    runTime: string;
+    theaters: string[];
 }
