de8de2de8669334c2f62a0bbab6edb7ae841222b
Phillip Stafford <phillip@philliprstafford.com>
2018-05-21 12:03:07 -0400

    feat: Add core methods to service

--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable ,  of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { ReportService } from './report.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class MoviedbService {
+  constructor(private http: HttpClient,
+    private reportService: ReportService) { }
 
-  constructor() { }
+    private apiKey = 'aeadd6ead4ce89db28f122189aaa9ed7';
+    private moviesUrl = 'api/moviedb';  // URL to web api
+
+    // Save this for when we go live
+
+    getMoviePosters(movieName) {
+
+      const url = 'https://api.themoviedb.org/3/search/movie?api_key='
+       + this.apiKey + '&language=en-US&query=' + movieName + '&page=1&include_adult=false';
+      return this.http.get(url);
+    }
+
+    /** GET Useres from the server */
+    getMovies(): Observable<string[]> {
+      return this.http.get<string[]>(this.moviesUrl)
+      .pipe(
+        tap(data => this.log(`MovieDB data received`))
+      );
+    }
+
+    // Logs the content to the reportService
+    private log(content: string) {
+      if (!content) { return; }
+      this.reportService.addReport( content );
+    }
 }
