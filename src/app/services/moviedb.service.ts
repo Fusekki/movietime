15fae5d06912fea4b8f38a06ac78880223f5bc09
Phillip Stafford <phillip@philliprstafford.com>
2018-05-21 12:45:53 -0400

    feat: Replace whitespace with special characters for movie title Add pipe to data retrieval

--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -15,22 +15,25 @@ export class MoviedbService {
     private reportService: ReportService) { }
 
     private apiKey = 'aeadd6ead4ce89db28f122189aaa9ed7';
-    private moviesUrl = 'api/moviedb';  // URL to web api
-
+    // private moviesUrl = 'api/moviedb';  // URL to web api
+    private moviesUrl = 'https://api.themoviedb.org/3/search/movie?api_key=';
     // Save this for when we go live
 
     getMoviePosters(movieName) {
-
-      const url = 'https://api.themoviedb.org/3/search/movie?api_key='
-       + this.apiKey + '&language=en-US&query=' + movieName + '&page=1&include_adult=false';
-      return this.http.get(url);
+      movieName = movieName.replace(/\s/g, '%20');
+      console.log(movieName);
+      const url = this.moviesUrl + this.apiKey + '&language=en-US&query=' + movieName + '&page=1&include_adult=false';
+      return this.http.get(url)
+      .pipe(
+        tap(data => this.log('MovieDB data received'))
+      );
     }
 
     /** GET Useres from the server */
     getMovies(): Observable<string[]> {
       return this.http.get<string[]>(this.moviesUrl)
       .pipe(
-        tap(data => this.log(`MovieDB data received`))
+        tap(data => this.log(`Movie data received`))
       );
     }
 
