a98319a851698b96590d2fa8220f138cc8b100cb
Phillip Stafford <phillip@philliprstafford.com>
2018-05-21 12:44:30 -0400

    refactor: Uncomment getMovies for api call

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -18,18 +18,18 @@ export class ApiService {
 
   // Save this for when we go live
 
-  // getMovies() {
-  //   const url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-05-16&zip=06820&api_key=3pb6pdpr5j5eeyucy9a5s5ua';
-  //   return this.http.get(url);
-  // }
+  getMovies() {
+    const url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-05-21&zip=06820&api_key=3pb6pdpr5j5eeyucy9a5s5ua';
+    return this.http.get(url);
+  }
 
   /** GET Useres from the server */
-  getMovies(): Observable<string[]> {
-    return this.http.get<string[]>(this.moviesUrl)
-    .pipe(
-      tap(data => this.log(`Movie data received`))
-    );
-  }
+  // getMovies(): Observable<string[]> {
+  //   return this.http.get<string[]>(this.moviesUrl)
+  //   .pipe(
+  //     tap(data => this.log(`Movie data received`))
+  //   );
+  // }
 
   // Logs the content to the reportService
   private log(content: string) {
