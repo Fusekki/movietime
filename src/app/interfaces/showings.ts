06c542999c353eda632c504e5016e47a8ec24588
Phillip Stafford <phillip@philliprstafford.com>
2018-05-23 13:14:57 -0400

    refactor: Move class definitions used for interace into file Moved file location subfolder interfaces

---
+++ src/app/interfaces/showings.ts
@@ -0,0 +1,57 @@
+export class PreferredImage {
+  width: string;
+  height: string;
+  uri: string;
+  category: string;
+  text: string;
+  primary: string;
+}
+
+export class Ratings {
+  body: string;
+  code: string;
+}
+
+export class QualityRating {
+  ratingsBody: string;
+  value: string;
+}
+
+export class Theatre {
+  id: string;
+  name: string;
+}
+
+
+export class Showtime {
+  theatre: Theatre[];
+  dateTime: string[];
+  barg: boolean;
+  ticketURI: string;
+}
+
+
+export interface Showings {
+  id: number;
+  tmsId: string;
+  rootId: number;
+  subType: string;
+  title: string;
+  releaseYear: string;
+  releaseDate: string;
+  titleLang: string;
+  descriptionLang: string;
+  entityType: string;
+  genres: string[];
+  longDescription: string;
+  shortDescription: string;
+  topCast: string[];
+  directors: string[];
+  officialUrl: string;
+  qualityRating: QualityRating;
+  ratings: Ratings;
+  advisories: string;
+  runTime: string;
+  preferredImage: PreferredImage;
+  showtimes: Showtime[];
+}
