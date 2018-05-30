237f1eb80a44b02c465dbf39f9a52f3d0aaeb3dc
Phillip Stafford <phillip@philliprstafford.com>
2018-05-30 14:24:38 -0400

    fix: Remove Movie class

--- src/app/interfaces/showings.ts
+++ src/app/interfaces/showings.ts
@@ -16,41 +16,6 @@ export class Ratings {
   code: string;
 }
 
-export class Movie {
-  constructor(t, st, rd, g, c, d, sd, ld, r, ad, rt)  {
-      this.title = t;
-      this.subType = st;
-      this.releaseDate = rd;
-      this.genres = g;
-      this.cast = c;
-      this.directors = d;
-      this.shortDescription = sd;
-      this.longDescription = ld;
-      this.rated = r;
-      this.advisories = ad;
-      this.runTime = rt;
-      this.theaters = [];
-      this.poster = null;
-      this.voteAverage = null;
-      this.popularity = null;
-   }
-  title: string;
-  subType: string;
-  releaseDate: string;
-  genres: string[];
-  cast: string[];
-  directors: string[];
-  shortDescription: string;
-  longDescription: string;
-  rated: string;
-  advisories: string[];
-  runTime: string;
-  theaters: Theater[];
-  poster: string;
-  voteAverage: string;
-  popularity: string;
-}
-
 export class QualityRating {
   ratingsBody: string;
   value: string;
