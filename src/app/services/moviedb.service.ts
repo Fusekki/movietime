3478686cb7afec792f53f916aa3098b1fa163ef6
Phillip Stafford <phillip@philliprstafford.com>
2018-06-01 15:37:42 -0400

    feat: Add getPeople function Add People interface to component

--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -3,8 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable ,  of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-import { Posters } from './../interfaces/posters';
 import { People } from './../interfaces/people';
+import { Posters } from './../interfaces/posters';
 
 import { ReportService } from './report.service';
 
@@ -30,22 +30,21 @@ export class MoviedbService {
       // console.log(url);
       return this.http.get<Posters>(url)
       .pipe(
-        tap(data => this.log('MovieDB Poster data received'))
+        tap(data => this.log('MovieDB poster received'))
       );
     }
 
-    getPeople(name): Observable<People> {
-      // Remove the whitespaces
-      name = name.replace(/\s/g, '%20');
-      const url = this.moviesUrl + 'person?api_key=' + this.apiKey + '&language=en-US&query=' + name
+    getPeople(person): Observable<People> {
+      // Remove the 3D from the end of title and replace the whitespaces
+      person = person.replace(/\s/g, '%20');
+      const url = this.moviesUrl + 'person?api_key=' + this.apiKey + '&language=en-US&query=' + person
        + '&page=1&include_adult=false';
       // console.log(url);
       return this.http.get<People>(url)
       .pipe(
-        tap(data => this.log('MovieDB Person data received'))
+        tap(data => this.log('MovieDB person received'))
       );
     }
-
 
     /** GET Useres from the server */
     getMovies(): Observable<string[]> {
