df8311cfbdb593bb09c6fbefcd1f2e71dc8996d0
Phillip Stafford <phillip@philliprstafford.com>
2018-06-01 10:16:09 -0400

    feat: Add getPeople function for API call to search people

--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -4,6 +4,7 @@ import { Observable ,  of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Posters } from './../interfaces/posters';
+import { People } from './../interfaces/people';
 
 import { ReportService } from './report.service';
 
@@ -18,20 +19,33 @@ export class MoviedbService {
 
     private apiKey = 'aeadd6ead4ce89db28f122189aaa9ed7';
     // private moviesUrl = 'api/moviedb';  // URL to web api
-    private moviesUrl = 'https://api.themoviedb.org/3/search/movie?api_key=';
+    private moviesUrl = 'https://api.themoviedb.org/3/search/';
     // Save this for when we go live
 
     getMoviePosters(movieName, releaseDate): Observable<Posters> {
       // Remove the 3D from the end of title and replace the whitespaces
       movieName = movieName.replace(/[3][D]/g, '').replace(/\s/g, '%20');
-      const url = this.moviesUrl + this.apiKey + '&language=en-US&query=' + movieName
+      const url = this.moviesUrl + 'movie?api_key=' + this.apiKey + '&language=en-US&query=' + movieName
        + '&page=1&include_adult=false&primary_release_year=' + releaseDate;
       // console.log(url);
       return this.http.get<Posters>(url)
       .pipe(
-        tap(data => this.log('MovieDB data received'))
+        tap(data => this.log('MovieDB Poster data received'))
       );
     }
+
+    getPeople(name): Observable<People> {
+      // Remove the whitespaces
+      name = name.replace(/\s/g, '%20');
+      const url = this.moviesUrl + 'person?api_key=' + this.apiKey + '&language=en-US&query=' + name
+       + '&page=1&include_adult=false';
+      // console.log(url);
+      return this.http.get<People>(url)
+      .pipe(
+        tap(data => this.log('MovieDB Person data received'))
+      );
+    }
+
 
     /** GET Useres from the server */
     getMovies(): Observable<string[]> {
