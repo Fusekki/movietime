9c7731ed4a575a7067736fdcc31697b3d650b957
Phillip Stafford <phillip@philliprstafford.com>
2018-06-03 12:45:08 -0400

    feat: Add videos interface with Video class

---
+++ src/app/interfaces/videos.ts
@@ -0,0 +1,15 @@
+class Video {
+  id: number;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+}
+
+export interface Videos {
+  id: number;
+  results: Video[];
+}
