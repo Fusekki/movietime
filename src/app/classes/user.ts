29c96493659a69c401d1e403f1bcf6e22bbb8537
Phillip Stafford <phillip@philliprstafford.com>
2018-06-05 13:04:06 -0400

    refactor: Change zipcode property to type string

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,8 +1,16 @@
 export class User {
+  constructor (id, e, f, l, z) {
+    this.id = id;
+    this.email = e;
+    this.firstName = f;
+    this.lastName = l;
+    this.zipcode = z;
+    this.theaters = [];
+  }
     id: number;
     email: string;
     firstName: string;
     lastName: string;
-    zipcode: number;
+    zipcode: string;
     theaters: number[];
   }
