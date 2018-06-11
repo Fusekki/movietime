c1ec3230ab0b3129bfa9e569044e8b29d912d345
Phillip Stafford <phillip@philliprstafford.com>
2018-06-11 11:07:35 -0400

    refactor: Change path from TheaterSearch  to Dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { TheaterSearchComponent } from './components/theater-search/theater-sear
 import { MovieSearchResultsComponent } from './components/movie-search-results/movie-search-results.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { MovieVideosComponent } from './components/movie-videos/movie-videos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  { path: ':user/profile', component: TheaterSearchComponent },
+  { path: ':user/profile', component: DashboardComponent },
   { path: ':user/movies/:zipcode', component: MovieSearchResultsComponent },
   { path: ':user/movies/:video/videos', component: MovieVideosComponent },
   // These defaults need to be last
