1a5183535f06c29463a0a8b3b9ddf236b97d9c97
Phillip Stafford <phillip@philliprstafford.com>
2018-06-12 15:15:25 -0400

    refactor: Change user to type <User> and add checkTheater function

--- src/app/components/theatre-times/theatre-times.component.ts
+++ src/app/components/theatre-times/theatre-times.component.ts
@@ -9,9 +9,12 @@ import { User } from '../../classes/user';
 })
 export class TheatreTimesComponent implements OnInit {
   @Input() theater: Theater;
-  @Input() user: User;
+  // @Input() user: User;
 
-  constructor(private ref: ChangeDetectorRef) { }
+  @Input() user: <User>;
+
+  constructor(private ref: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
     this.log();
@@ -19,7 +22,7 @@ export class TheatreTimesComponent implements OnInit {
   }
 
   toggle(e, id): void {
-    // console.log('toggled');
+    console.log('toggled');
     // console.log(e);
     // console.log(id);
     if (e.checked) {
@@ -27,40 +30,62 @@ export class TheatreTimesComponent implements OnInit {
     } else {
       this.removeTheater(id);
     }
-    ref.detectChanges();
+    this.ref.detectChanges();
   }
 
   addTheater(theater) {
     console.log('adding theater');
+    console.log(this.user.theaters);
     if (this.user.theaters == null) {
-      this.user.theaters = [theater];
+      this.user.theaters = [theater.id];
       // Note: Need to find a way to update all theaters with this checkmark
       theater.checked = true;
       console.log(this.user.theaters);
     } else if (this.user.theaters.indexOf(theater.id)) {
             // console.log('Theater not in list.');
-      // console.log('Adding theater ' + theater.id);
+      console.log('Adding theater ' + theater.id);
       this.user.theaters.push(theater.id);
       theater.checked = true;
     } else {
-      // console.log('Already in list');
+      console.log('Already in list');
     }
   }
 
   removeTheater(theater) {
-    const idx = this.user.theaters.indexOf(theater.id);
-    console.log(idx);
-    if (idx > -1 ) {
-      // console.log('Removing theater ' + theater.id);
+    console.log('Removing theater');
+    console.log(theater);
+    console.log(this.user.theaters);
+
+    if (this.user.theaters == null) {
+      console.log('list is empty. creating new one.');
+      this.user.theaters = [theater];
+      // Note: Need to find a way to update all theaters with this checkmark
+      theater.checked = true;
+      console.log(this.user.theaters);
+    } else if (this.user.theaters.indexOf(theater.id) > -1) {
+      const idx = this.user.theaters.indexOf(theater.id);
+      console.log(idx);
+      console.log('Removing theater ' + theater.id);
       this.user.theaters.splice(idx, 1);
       theater.checked = false;
     } else {
-      // console.log('Not in list');
+      console.log(this.user.theaters.indexOf(theater.id));
+        console.log('Not in list');
     }
   }
 
   log(): void {
     console.log('theater-times component loaded.')
+  }
+
+  checkTheater(theater) {
+    if (this.user.theaters == null) {
+      return false;
+      console.log(this.user.theaters);
+    } else if (this.user.theaters.indexOf(theater.id) > -1) {
+      return true;
+    }
+    return false;
   }
 
 
