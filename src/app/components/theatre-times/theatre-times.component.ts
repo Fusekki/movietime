608dd18259ca71bc0471edd3f79c54016ddb7465
Phillip Stafford <phillip@philliprstafford.com>
2018-06-13 11:13:38 -0400

    refactor: Comment out old code

--- src/app/components/theatre-times/theatre-times.component.ts
+++ src/app/components/theatre-times/theatre-times.component.ts
@@ -16,64 +16,64 @@ export class TheatreTimesComponent implements OnInit {
 
   ngOnInit() {
     this.log();
-    console.log(this.theater);
+    // console.log(this.theater);
   }
 
-  toggle(e, id): void {
-    console.log('toggled');
-    // console.log(e);
-    // console.log(id);
-    if (e.checked) {
-      this.addTheater(id);
-    } else {
-      this.removeTheater(id);
-    }
-    this.ref.detectChanges();
-  }
+  // toggle(e, id): void {
+  //   console.log('toggled');
+  //   // console.log(e);
+  //   // console.log(id);
+  //   if (e.checked) {
+  //     this.addTheater(id);
+  //   } else {
+  //     this.removeTheater(id);
+  //   }
+  //   this.ref.detectChanges();
+  // }
 
   addTheater(theater) {
-    console.log('adding theater');
-    console.log(this.user.theaters);
+    // console.log('adding theater');
+    // console.log(this.user.theaters);
     if (this.user.theaters == null) {
       this.user.theaters = [theater.id];
       // Note: Need to find a way to update all theaters with this checkmark
       theater.checked = true;
-      console.log(this.user.theaters);
+      // console.log(this.user.theaters);
     } else if (this.user.theaters.indexOf(theater.id)) {
             // console.log('Theater not in list.');
-      console.log('Adding theater ' + theater.id);
+      // console.log('Adding theater ' + theater.id);
       this.user.theaters.push(theater.id);
       theater.checked = true;
     } else {
-      console.log('Already in list');
+      // console.log('Already in list');
     }
   }
 
   removeTheater(theater) {
-    console.log('Removing theater');
-    console.log(theater);
-    console.log(this.user.theaters);
+    // console.log('Removing theater');
+    // console.log(theater);
+    // console.log(this.user.theaters);
 
     if (this.user.theaters == null) {
-      console.log('list is empty. creating new one.');
+      // console.log('list is empty. creating new one.');
       this.user.theaters = [theater];
       // Note: Need to find a way to update all theaters with this checkmark
       theater.checked = true;
-      console.log(this.user.theaters);
+      // console.log(this.user.theaters);
     } else if (this.user.theaters.indexOf(theater.id) > -1) {
       const idx = this.user.theaters.indexOf(theater.id);
-      console.log(idx);
-      console.log('Removing theater ' + theater.id);
+      // console.log(idx);
+      // console.log('Removing theater ' + theater.id);
       this.user.theaters.splice(idx, 1);
       theater.checked = false;
     } else {
-      console.log(this.user.theaters.indexOf(theater.id));
-        console.log('Not in list');
+      // console.log(this.user.theaters.indexOf(theater.id));
+        // console.log('Not in list');
     }
   }
 
   log(): void {
-    console.log('theater-times component loaded.')
+    console.log('theater-times component loaded.');
   }
 
   checkTheater(theater) {
@@ -86,6 +86,5 @@ export class TheatreTimesComponent implements OnInit {
     }
     return false;
   }
-
 
 }
