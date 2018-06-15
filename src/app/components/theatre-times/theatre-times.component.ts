b9adb748c28538374214e0b6ba01a08312694b3b
Phillip Stafford <phillip@philliprstafford.com>
2018-06-15 19:30:02 -0400

    refactor: Remove old code

--- src/app/components/theatre-times/theatre-times.component.ts
+++ src/app/components/theatre-times/theatre-times.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectorRef, OnInit, Input } from '@angular/core';
-import { Observable ,  of } from 'rxjs';
 import { Theater } from '../../classes/movie';
 import { User } from '../../classes/user';
+import { tap, map } from 'rxjs/operators';
 
 import {UserService} from '../../services/user.service';
 
@@ -22,78 +22,38 @@ export class TheatreTimesComponent implements OnInit {
 
   ngOnInit() {
     this.log();
-    // this.changesDetected = false;
-    // console.log(this.changesDetected);
-    // const nIntervId = setInterval(this.checkForChanges, 10000);
-    // console.log(this.theater);
   }
 
-  // toggle(e, id): void {
-  //   console.log('toggled');
-  //   // console.log(e);
-  //   // console.log(id);
-  //   if (e.checked) {
-  //     this.addTheater(id);
-  //   } else {
-  //     this.removeTheater(id);
-  //   }
-  //   this.ref.detectChanges();
-  // }
-
-  addTheater(theater) {
-    // console.log('adding theater');
-    // console.log(this.user.theaters);
+  addTheater(theater: Theater) {
     if (this.user.theaters == null) {
       this.user.theaters = [theater.id];
-      // Note: Need to find a way to update all theaters with this checkmark
       theater.checked = true;
-      // console.log(this.user.theaters);
     } else if (this.user.theaters.indexOf(theater.id)) {
-            // console.log('Theater not in list.');
-      // console.log('Adding theater ' + theater.id);
       this.user.theaters.push(theater.id);
       theater.checked = true;
-    } else {
-      // console.log('Already in list');
     }
-    console.log('Theater added.   Changes detected.');
-    // this.changesDetected = true;
-    // Update the database
-    console.log('updatind user.');
+    console.log('updating user.');
     console.log(this.user);
-    this.userService.updateUser(this.user);
-
+    this.userService.updateUser((this.user) as User)
+    .subscribe(() => console.log('Updated User'));
   }
-
-  removeTheater(theater) {
-    // console.log('Removing theater');
-    // console.log(theater);
-    // console.log(this.user.theaters);
-
+  removeTheater(theater: Theater) {
     if (this.user.theaters == null) {
       // console.log('list is empty. creating new one.');
-      this.user.theaters = [theater];
-      // Note: Need to find a way to update all theaters with this checkmark
+      this.user.theaters = [theater.id];
       theater.checked = true;
-      // console.log(this.user.theaters);
     } else if (this.user.theaters.indexOf(theater.id) > -1) {
       const idx = this.user.theaters.indexOf(theater.id);
-      // console.log(idx);
-      // console.log('Removing theater ' + theater.id);
+
       this.user.theaters.splice(idx, 1);
       theater.checked = false;
-    } else {
-      // console.log(this.user.theaters.indexOf(theater.id));
-        // console.log('Not in list');
     }
-    // this.changesDetected = true;
-        // Update the database
-        console.log('updatind user.');
-        console.log(this.user);
-        this.userService.updateUser(this.user);
+      // Update the database
+      this.userService.updateUser((this.user) as User)
+      .subscribe(() => console.log('Updated User'));
   }
 
-  checkTheater(theater) {
+  checkTheater(theater: Theater) {
     if (this.user.theaters == null) {
       return false;
     }
