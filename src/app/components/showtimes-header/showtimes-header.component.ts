7fed930b5e93bdeaa2d0304dc6bca01dd3359150
Phillip Stafford <phillip@philliprstafford.com>
2018-06-19 15:04:35 -0400

    refactor: Add zipcode Input of type number Import Input

--- src/app/components/showtimes-header/showtimes-header.component.ts
+++ src/app/components/showtimes-header/showtimes-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-showtimes-header',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showtimes-header.component.css']
 })
 export class ShowtimesHeaderComponent implements OnInit {
+@Input() zipcode: number;
 
   constructor() { }
 
