4ebf6a2f96c8a026a36c231086c1083a715367a7
Phillip Stafford <phillip@philliprstafford.com>
2018-06-20 14:50:25 -0400

    refactor: Add Theater, Showings, ApiService and TheaterService
    Add private variables and constructor. Create @Input set function that
    chains getMovies function when user is asigned.

--- src/app/components/theater-list/theater-list.component.ts
+++ src/app/components/theater-list/theater-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../classes/user';
+import { Theater, Movie } from '../../classes/theater';
+import { Showings } from '../../interfaces/showings';
+
+import { ApiService } from '../../services/api.service';
+import { TheaterService } from '../../services/theater.service';
 
 // import { User } from '../../classes/user';
 
@@ -9,11 +14,45 @@ import { User } from '../../classes/user';
   styleUrls: ['./theater-list.component.css']
 })
 export class TheaterListComponent implements OnInit {
-  @Input() user: User;
-  constructor() { }
+  // @Input() user: User;
+  private _movies: Array<Movie>;
+  private _theaters: Array<Theater>;
+  private _user: User;
+
+  constructor(
+    private theaterService: TheaterService,
+    private apiService: ApiService
+  ) {
+    this._theaters = new Array<Theater>();
+  }
 
   ngOnInit() {
     this.log();
+    // this.getMovies()
+    // .subscribe((data: Showings[]) => {
+    //   this._theaters = this.parseTheaters(data, this.user.theaters);
+    // });
+  }
+
+  @Input()
+  set user(user: User) {
+    if (user) {
+      this._user = user;
+      this.getMovies()
+        .subscribe((data: Showings[]) => {
+          this._theaters = this.parseTheaters(data, this._user.theaters);
+          console.log(this._theaters);
+        });
+    }
+
+  }
+
+  getMovies(): any {
+    return this.apiService.getMovies();
+  }
+
+  parseTheaters(data, theaters): any {
+    return this.theaterService.parseTheaters(data, theaters);
   }
 
   log(): void {
