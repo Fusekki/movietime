30474e6b8850b3f0820ccdf74f5ac5a726725da2
Phillip Stafford <phillip@philliprstafford.com>
2018-06-22 18:21:04 -0400

    refactor: Add additiional logic to incomplete parseTheaters function

--- src/app/services/theater.service.ts
+++ src/app/services/theater.service.ts
@@ -16,25 +16,57 @@ export class TheaterService {
   parseTheaters(showings, theaterList?) {
     let target = new Array<number>();
     showings.map(showing => {
-      console.log(showing.title);
+      // console.log(showing);
       showing.showtimes.map(show => {
+        // console.log(show);
         const id = parseInt(show.theatre.id, 10);
         console.log(theaterList.indexOf(id > -1));
         if (theaterList.indexOf(id) === -1) {
           // Theater is not in the array, push it and the movie as a new object.
           console.log('Theater is a favorite.');
+          console.log(show.theatre.name);
           // Check if the theater has been added to the array
           let isTheater = this.theaters.some(theater => theater.id === show.theatre.id);
+          console.log(isTheater);
           if (isTheater) {
+            console.log(this.theaters);
             console.log('Theater is in array.');
             console.log('Checking if movie exists in array.');
+            console.log('Searching for a title with the name of ' + showing.title);
+            console.log('At theater with name of ' + show.theatre.name);
+            let t = this.theaters.find(theater => theater.id === show.theatre.id);
+            console.log(t);
+            if (t.movies.some(movie => movie.title === showing.title)) {
+              t = t.movies.find(movie => movie.title === showing.title);
+              console.log(t);
+            } else {
+              console.log('Nothing to do here.');
+            }
+                  // theater.movies.find(movie => movie.title === showing.title);
+                // });
+            // let test = this.theaters.find(theater => theaters.movies.length )
             let isMovie = this.theaters.some(theater => theater.movies.some(movie => movie.title === showing.title));
-            if (isMovie) {
+            console.log(isMovie);
+            console.log(this.theaters.find(theater => theater.movies.some(movie => movie.title === showing.title)));
+            console.log(showing.title);
+            // This resolves to the id of the matching theater
+            let theaterIdx = this.theaters.find(theater => theater.id === show.theatre.id);
+            if (isMovie) { // here checking it
               // Theater exists.  Let's check if the movie exists as well.
+              console.log(showing.title);
+              console.log(theaterIdx);
               console.log('Movie is in array. Need to add the time to the Movie object.');
+              console.log(show.dateTime);
+              let movieIdx = theaterIdx.movies.find(movie => movie.title === showing.title);
+              console.log(theaterIdx);
+              console.log(show);
+              // movieIdx.times.push(show.dateTime);
+              let v = show.dateTime;
+              movieIdx.times.push(v);
+              // console.log(movieIdx);
+              // console.log(movieIdx.times);
             } else {
               console.log('Movie is not in array.  Need to add movie to theater array.');
-              let theaterIdx = this.theaters.find(theater => theater.id === show.theatre.id);
               theaterIdx.movies.push(this.createMovie(show, showing));
             }
           } else {
