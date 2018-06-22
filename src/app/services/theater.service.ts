5506f2d6e6a0c3b8648589a54b3a4ead6ff2f6ba
Phillip Stafford <phillip@philliprstafford.com>
2018-06-22 11:37:14 -0400

    refactor: Add createMovie function  and add the function call in the if/else chain

--- src/app/services/theater.service.ts
+++ src/app/services/theater.service.ts
@@ -18,22 +18,24 @@ export class TheaterService {
     showings.map(showing => {
       console.log(showing.title);
       showing.showtimes.map(show => {
-        let id = parseInt(show.theatre.id, 10);
+        const id = parseInt(show.theatre.id, 10);
         console.log(theaterList.indexOf(id > -1));
         if (theaterList.indexOf(id) === -1) {
           // Theater is not in the array, push it and the movie as a new object.
           console.log('Theater is a favorite.');
           // Check if the theater has been added to the array
-          let isPresent = this.theaters.some(theater => theater.id === show.theatre.id);
-          if (isPresent) {
+          let isTheater = this.theaters.some(theater => theater.id === show.theatre.id);
+          if (isTheater) {
             console.log('Theater is in array.');
             console.log('Checking if movie exists in array.');
-            isPresent = this.theaters.some(theater => theater.movies.some(movie => movie.title === showing.title));
-            if (isPresent) {
+            let isMovie = this.theaters.some(theater => theater.movies.some(movie => movie.title === showing.title));
+            if (isMovie) {
               // Theater exists.  Let's check if the movie exists as well.
               console.log('Movie is in array. Need to add the time to the Movie object.');
             } else {
               console.log('Movie is not in array.  Need to add movie to theater array.');
+              let theaterIdx = this.theaters.find(theater => theater.id === show.theatre.id);
+              theaterIdx.movies.push(this.createMovie(show, showing));
             }
           } else {
             // Theater is not present
@@ -70,5 +72,23 @@ export class TheaterService {
     console.log(t);
 
     return t;
+  }
+
+  createMovie(s,g) {
+    let m = new Movie(
+      g.title,
+      g.subType,
+      g.releaseDate,
+      g.genres,
+      g.topCast,
+      g.directors,
+      g.shortDescription,
+      g.longDescription,
+      g.ratings,
+      g.advisories,
+      g.runTime,
+      s.dateTime);
+    console.log('Movie added ');
+    return m;
   }
 }
