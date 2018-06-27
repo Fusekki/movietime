86521c23ea55ea80c10a363a5716720b2c2d4dbd
Phillip Stafford <phillip@philliprstafford.com>
2018-06-27 11:27:04 -0400

    feat: Add checkTheater function and @Input User

--- src/app/components/theater-item/theater-item.component.ts
+++ src/app/components/theater-item/theater-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Theater, Movie} from './../../classes/movie';
+import { User } from './../../classes/user';
 
 @Component({
   selector: 'app-theater-item',
@@ -8,10 +9,31 @@ import { Theater, Movie} from './../../classes/movie';
 })
 export class TheaterItemComponent implements OnInit {
 @Input() theater: Theater;
+@Input() user: User;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  checkTheater(theater: Theater) {
+
+      console.log(this.user);
+
+      // Convert to int
+      theater.id = +theater.id;
+
+      if (this.user.theaters == null) {
+        return false;
+      }
+
+      // console.log(this.user.theaters.indexOf(theater.id));
+      if (this.user.theaters.indexOf(theater.id) > -1) {
+        console.log('match.');
+        return true;
+      } else {
+        console.log(' no match.');
+        return false;
+      }
+  }
 }
