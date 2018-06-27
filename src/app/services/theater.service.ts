5607f1c767b35722f8bf9d74dc1cbfe6affb9e7d
Phillip Stafford <phillip@philliprstafford.com>
2018-06-27 10:19:53 -0400

    refactor: Remove old unneeded code

--- src/app/services/theater.service.ts
+++ src/app/services/theater.service.ts
@@ -17,25 +17,15 @@ export class TheaterService {
     console.log(showings);
     let target = new Array<number>();
     showings.map(showing => {
-      // console.log(showing);
       showing.showtimes.map(show => {
-        // console.log(show);
         const id = parseInt(show.theatre.id, 10);
-        // console.log(theaterList.indexOf(id > -1));
         if (theaterList.indexOf(id) === -1) {
           // Theater is not in the array, push it and the movie as a new object.
           console.log('Theater is a favorite.');
           console.log(show.theatre.name);
           // Check if the theater has been added to the array
           let ifTheater = this.theaters.some(theater => theater.id === show.theatre.id);
-          // console.log(ifTheater);
           if (ifTheater) {
-            // console.log('Current list of this.theaters:');
-            // console.log(this.theaters);
-            // console.log('Theater is in array this.theaters.');
-            // console.log('Checking if movie exists in array.');
-            // console.log('Searching for a title with the name of ' + showing.title);
-            // console.log('At theater with name of ' + show.theatre.name);
             let t = this.theaters.find(theater => theater.id === show.theatre.id);
             console.log('Theater object already created:');
             console.log(t);
@@ -43,8 +33,7 @@ export class TheaterService {
               let m = t.movies.find(movie => movie.title === showing.title);
               console.log('Movie object already created : ');
               console.log(m);
-              // console.log(show);
-              // console.log(showings);
+
               m.times.push(show.dateTime);
               console.log('New movie object :');
               console.log(m);
@@ -53,42 +42,8 @@ export class TheaterService {
               console.log(show);
               t.movies.push(this.createMovie(show, showing));
             }
-              // theater.movies.find(movie => movie.title === showing.title);
-              // });
-            // let test = this.theaters.find(theater => theaters.movies.length )
-            // let isTheater = this.theaters.some(theater => theater.movies.some(movie => movie.title === showing.title));
-            // console.log(isTheater);
-            // console.log(this.theaters.find(theater => theater.movies.some(movie => movie.title === showing.title)));
-            // console.log(showing.title);
-            // This resolves to the id of the matching theater
-            // let theaterIdx = this.theaters.find(theater => theater.id === show.theatre.id);
-            // if (isTheater) { // here checking it
-            //   // Theater exists.  Let's check if the movie exists as well.
-            //   console.log('Theater')
-            //   console.log(showing.title);
-            //   console.log(theaterIdx);
-            //   console.log('Movie is in array. Need to add the time to the Movie object.');
-            //   console.log(show.dateTime);
-            //   let movieIdx = theaterIdx.movies.find(movie => movie.title === showing.title);
-            //   console.log(theaterIdx);
-            //   console.log(show);
-            //   console.log(movieIdx);
-            //   if (!movieIdx === undefined) {
-            //     let v = show.dateTime;
-            //     movieIdx.times.push(v);
-            //   }
-
-              // movieIdx.times.push(show.dateTime);
-
-              // console.log(movieIdx);
-              // console.log(movieIdx.times);
-            // } else {
-            //   console.log('Movie is not in array.  Need to add movie to theater array.');
-            //   theaterIdx.movies.push(this.createMovie(show, showing));
-            // }
           } else {
             // Theater is not present
-
             console.log('Creating theater object and pushing to this.theaters.');
             console.log('Theater name : ' + show.theatre.name);
             console.log('Theater time : ' + show.dateTime);
@@ -119,16 +74,11 @@ export class TheaterService {
       g.advisories,
       g.runTime,
       s.dateTime);
-    // console.log('Movie added ');
-    // console.log(s);
-    // console.log(g);
     let t = new Theater(
       s.theatre.name,
       s.theatre.id,
       m
     );
-    // console.log(t);
-
     return t;
   }
 
@@ -146,7 +96,6 @@ export class TheaterService {
       g.advisories,
       g.runTime,
       s.dateTime);
-    // console.log('Movie added ');
     return m;
   }
 }
